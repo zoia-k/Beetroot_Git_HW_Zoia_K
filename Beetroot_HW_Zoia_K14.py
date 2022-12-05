035872e24653bb6cab83a6b456221496b987a7f3
Zoia-K <evahekate@yahoo.com>
2022-12-05 10:25:16 +0200

    Task 2 added

--- Beetroot_HW_Zoia_K14.py
+++ Beetroot_HW_Zoia_K14.py
@@ -13,7 +13,7 @@ def logger(func):
     @wraps(func)
     def wrap(*args):
         print(f'{func.__name__} called with {args}')
-        func()
+        func(*args)
 
     return wrap
 
@@ -23,11 +23,41 @@ def add(x, y):
     return x + y
 
 
-# add(4, 5)
+add(4, 5)
 
 
 @logger
 def square_all(*args):
     return [arg ** 2 for arg in args]
 
-# square_all(1, 2, 3, 4)
+
+square_all(1, 2, 3, 4)
+
+# Task 2
+# Write a decorator that takes a list of stop words
+# and replaces them with * inside the decorated function
+
+
+def stop_words(words_list=[]):
+    def check_slogan(func):
+        def check_words(arg):
+            slogan = func(arg)
+            print(slogan)
+            for word in words_list:
+                slogan = slogan.replace(word, '*')
+            print(slogan)
+            return slogan
+
+        return check_words
+
+    return check_slogan
+
+
+@stop_words(['pepsi', 'BMW'])
+def create_slogan(name: str) -> str:
+    return f"{name} drinks pepsi in his brand new BMW!"
+
+
+assert create_slogan("Steve") == "Steve drinks * in his brand new *!"
+assert create_slogan("Jane") == "Jane drinks * in his brand new *!"
+
