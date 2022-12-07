f49dff210accb431c77810ad38249a536b389add
Zoia-K <evahekate@yahoo.com>
2022-12-07 13:46:41 +0200

    Task 3 added one more test

--- Beetroot_HW_Zoia_K15.py
+++ Beetroot_HW_Zoia_K15.py
@@ -46,3 +46,73 @@ class Dog:
 dog_1 = Dog(12)
 
 print(dog_1.human_age())
+
+
+# Task 3
+
+# TV controller
+#
+# Create a simple prototype of a TV controller in Python. It’ll use the following commands:
+#
+# first_channel() - turns on the first channel from the list.
+# last_channel() - turns on the last channel from the list.
+# turn_channel(N) - turns on the N channel. Pay attention that the channel numbers start from 1, not from 0.
+# next_channel() - turns on the next channel. If the current channel is the last one, turns on the first channel.
+# previous_channel() - turns on the previous channel. If the current channel is the first one, turns on the last channel.
+# current_channel() - returns the name of the current channel.
+# is_exist(N/'name') - gets 1 argument - the number N or the string 'name' and returns "Yes", if the channel N
+#                       or 'name' exists in the list, or "No" - in the other case.
+
+CHANNELS = ["BBC", "Discovery", "TV1000"]
+
+
+class TVController:
+
+    def __init__(self, channels=CHANNELS):
+        self.channels = channels
+        self.number = 0
+
+    def first_channel(self):
+        return self.channels[0]
+
+    def last_channel(self):
+        self.number = len(self.channels)
+        return self.channels[-1]
+
+    def turn_channel(self, num):
+        self.number = num - 1
+        print(self.number)
+        return self.channels[self.number]
+
+    def next_channel(self):
+        self.number = (self.number % len(self.channels)) + 1
+        print(self.number)
+        return self.channels[self.number]
+
+    def previous_channel(self):
+        self.number = (self.number % len(self.channels)) - 1
+        print(self.number)
+        return self.channels[self.number]
+
+    def current_channel(self):
+        return self.channels[self.number]
+
+    def is_exist(self, name):
+        if name in self.channels:
+            return "Yes"
+        return "No"
+
+
+print("==============")
+controller = TVController(CHANNELS)
+
+print(controller.first_channel() == "BBC")
+print(controller.last_channel() == "TV1000")
+print(controller.turn_channel(1) == "BBC")
+print(controller.next_channel() == "Discovery")
+print(controller.previous_channel() == "BBC")
+print(controller.current_channel() == "BBC")
+print(controller.is_exist("BBC") == "Yes")
+print(controller.is_exist("CNN") == "No")
+
+
