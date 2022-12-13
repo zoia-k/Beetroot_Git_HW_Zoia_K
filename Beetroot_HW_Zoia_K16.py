366c43f2ace000bdee459b1f16115cdc75186711
Zoia-K <evahekate@yahoo.com>
2022-12-13 14:20:57 +0200

    Task 3 added

--- Beetroot_HW_Zoia_K16.py
+++ Beetroot_HW_Zoia_K16.py
@@ -110,3 +110,121 @@ assert m.square_nums([7, 11, 5, 4]) == [49, 121, 25, 16]
 assert m.remove_positives([26, -11, -8, 13, -90]) == [-11, -8, -90]
 
 assert m.filter_leaps([2001, 1884, 1995, 2003, 2020]) == [1884, 2020]
+
+# Task 3
+''' 
+Product Store
+
+Write a class Product that has three attributes:
+
+type
+name
+price
+Then create a class ProductStore, which will have some Products and will operate with all products in the store. 
+All methods, in case they can’t perform its action, should raise ValueError with appropriate error information.
+
+Tips: Use aggregation/composition concepts while implementing the ProductStore class. 
+You can also implement additional classes to operate on a certain type of product, etc.
+
+Also, the ProductStore class must have the following methods:
+
+add(product, amount) - adds a specified quantity of a single product with a predefined price premium for your store(30 percent)
+set_discount(identifier, percent, identifier_type=’name’) - adds a discount for all products specified by input identifiers (type or name). 
+                                                            The discount must be specified in percentage
+sell_product(product_name, amount) - removes a particular amount of products from the store if available, in other case raises an error. 
+                                    It also increments income if the sell_product method succeeds.
+get_income() - returns amount of many earned by ProductStore instance.
+get_all_products() - returns information about all available products in the store.
+get_product_info(product_name) - returns a tuple with product name and amount of items in the store.
+
+'''
+
+
+class Product:
+    def __init__(self, type_, name, price):
+        self.type = type_
+        self.name = name
+        self.price = price
+
+
+class ProductStore:
+    def __init__(self):
+        self._income = 0
+        self._products = []
+        self.name_discounts = {}
+        self.type_discounts = {}
+
+
+    def add(self, product, amount=0):
+        self._products.append((product, amount))
+
+    def set_discount(self, identifier, percent, identifier_type='name'):
+        if identifier_type == 'name':
+            self.name_discounts[identifier] = percent
+        elif identifier_type == 'type':
+            self.type_discounts[identifier] = percent
+        else:
+            raise ValueError("Invalid identifier_type")
+
+    def get_product_info(self, product_name):
+        items = 0
+        for product, amount in self._products:
+            if product_name == product.name:
+                items += amount
+
+        return product_name, items
+
+    def sell_product(self, product_name, amount):
+        if self.get_product_info(product_name)[1] < amount:
+            raise ValueError(f'Not enough items of {product_name}')
+        to_remove = amount
+        for i in range(len(self._products)):
+            product, amount = self._products[i]
+            if product.name == product_name:
+                will_remove = min(amount, to_remove)
+                self._products[i] = (product, amount - will_remove)
+                to_remove -= will_remove
+                self._income += self.get_selling_price(product)
+                if to_remove == 0:
+                    break
+
+        self._products = [(product, amount) for product, amount in self._products if amount > 0]
+
+    def get_all_products(self):
+        return [(product, amount) for product, amount in self._products]
+
+    def get_income(self):
+        return self._income
+
+    def get_selling_price(self, product):
+        name_discount = self.name_discounts.get(product.name, 0)
+        type_discount = self.type_discounts.get(product.name, 0)
+        return product.price * 1.3 * (1 - name_discount/100.0) * (1 - type_discount/100.0)
+
+
+
+
+
+
+s = ProductStore()
+p = Product('Sport', 'Football T-Shirt', 100)
+p2 = Product('Food', 'Ramen', 1.5)
+
+s.add(p, 10)
+s.add(p, 5)
+print(s._products)
+
+s.set_discount("Food", 20, "type")
+s.set_discount("Sport", 30, "type")
+s.set_discount("Ramen", 10)
+
+print(s.name_discounts)
+print(s.type_discounts)
+
+s.add(p, 10)
+
+s.add(p2, 300)
+
+s.sell_product('Ramen', 10)
+
+assert s.get_product_info('Ramen') == ('Ramen', 290)
