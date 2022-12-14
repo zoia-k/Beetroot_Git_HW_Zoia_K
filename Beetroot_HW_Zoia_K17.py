65c89cd17912a5aa0967a272f99faebb397e8d60
Zoia-K <evahekate@yahoo.com>
2022-12-14 15:23:54 +0200

    Task 2 added

--- Beetroot_HW_Zoia_K17.py
+++ Beetroot_HW_Zoia_K17.py
@@ -39,3 +39,102 @@ dog = Dog()
 animal_talks(cat)
 animal_talks(dog)
 
+# Task 2
+'''
+Library
+
+Write a class structure that implements a library. Classes:
+1) Library - name, books = [], authors = []
+2) Book - name, year, author (author must be an instance of Author class)
+3) Author - name, country, birthday, books = []
+
+Library class
+Methods:
+
+- new_book(name: str, year: int, author: Author) - returns an instance of Book class and 
+        adds the book to the books list for the current library.
+- group_by_author(author: Author) - returns a list of all books grouped by the specified author
+- group_by_year(year: int) - returns a list of all the books grouped by the specified year
+
+All 3 classes must have a readable __repr__ and __str__ methods.
+
+Also, the book class should have a class variable which holds the amount of all existing books
+'''
+
+
+class Author:
+    def __init__(self, name, country, birthday, books=[]):
+        self.name = name
+        self.country = country
+        self.birthday = birthday
+        self.books = books
+
+    def __str__(self):
+        return f'{self.name}, {self.country}, {self.birthday}'
+
+    def __repr__(self):
+        return f'Author: {self.name}, {self.country}, {self.birthday}'
+
+
+class Book:
+    counter = 0
+
+    def __init__(self, name, year, author: Author):
+        self.name = name
+        self.year = year
+        self.author = author
+        Book.counter += 1
+
+    def __str__(self):
+        return f"'{self.name}' by {self.author.name}, {self.year}"
+
+    def __repr__(self):
+        return f"Book: '{self.name}', written by {self.author.name} in {self.year}"
+
+
+class Library:
+
+    def __init__(self, name, books=[], authors=[]):
+        self.name = name
+        self.books = books
+        self.authors = authors
+
+    def __str__(self):
+        return f'{self.name},{self.books}, {self.authors}'
+
+    def __repr__(self):
+        return f'{self.name},{self.books}, {self.authors}'
+
+    def new_book(self, name: str, year: int, author: Author) -> Book:
+        book = Book(name, year, author)
+        author.books.append(book)
+        self.books.append(book)
+        if author not in self.authors:
+            self.authors.append(author)
+        return book
+
+    def group_by_author(self, author: Author):
+        author_sorted_books = []
+        for book in self.books:
+            if book.author.name == author:
+                author_sorted_books.append(book)
+        return author_sorted_books
+
+    def group_by_year(self, year: int):
+        year_sorted_books = []
+        for book in self.books:
+            if book.year == year:
+                year_sorted_books.append(book)
+        return year_sorted_books
+
+
+library = Library('School Library')
+author_1 = Author('Stiven King', 'USA', '21/09/1947')
+
+print(library.new_book('Christine', 1983, author_1))
+print(library.new_book('The Girl Who Loved Tom Gordon', 1999, author_1))
+print(Book.counter)
+print(library)
+print(library.group_by_author('Stiven King'))
+print(library.group_by_year(1983))
+print(library.group_by_year(1999))
