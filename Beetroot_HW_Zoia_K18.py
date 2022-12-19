67ec1580c259645425b89c989a23738a8d5c42b5
Zoia-K <evahekate@yahoo.com>
2022-12-19 18:56:26 +0200

    Task 2 added

--- Beetroot_HW_Zoia_K18.py
+++ Beetroot_HW_Zoia_K18.py
@@ -51,3 +51,60 @@ class Validator:
 
 
 Validator('AB.yz@mail.com')
+
+
+# Task 2
+
+# Bosses and Workers
+
+# Implement 2 classes, the first one is the Boss and the second one is the Worker.
+# Worker has a property 'boss', and its value must be an instance of Boss.
+# You can reassign this value, but you should check whether the new value is Boss.
+# Each Boss has a list of his own workers.
+# You should implement a method that allows you to add workers to a Boss.
+# You're not allowed to add instances of Boss class to workers list directly via access to attribute, use getters and setters instead!
+
+
+class Boss:
+
+    def __init__(self, id_: int, name: str, company: str):
+        self.id = id_
+        self.name = name
+        self.company = company
+        self.workers = []
+
+
+class Worker:
+
+    def __init__(self, id_: int, name: str, company: str, boss: Boss):
+        self.id = id_
+        self.name = name
+        self.company = company
+        self._boss = boss
+        boss.workers.append(self)
+
+    def get_boss(self):
+        return self._boss
+
+    def set_boss(self, new_boss):
+        self._boss.workers.remove(self)
+        self._boss = new_boss
+        new_boss.workers.append(self)
+
+
+boss_1 = Boss(125412, 'Boss_1', 'Bad Company')
+worker_1 = Worker(741852, 'Worker_1', boss_1.company, boss_1)
+
+assert worker_1.get_boss() == boss_1
+assert boss_1.workers[0] == worker_1
+
+boss_2 = Boss(296412, 'Boss_2', 'Good Company')
+worker_2 = Worker(741482, 'Worker_2', 'Good Company', boss_2)
+
+worker_1.set_boss(boss_2)
+
+assert worker_2.get_boss() == boss_2
+assert boss_2.workers[1] == worker_1
+assert len(boss_1.workers) == 0
+
+#print(boss_2.workers)
